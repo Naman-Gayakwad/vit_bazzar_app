f32fba012cf2b04f210f90b0556de878f3a9b08e
Naman Gayakwad <110586020+Naman-Gayakwad@users.noreply.github.com>
2024-02-09 23:24:40 +0530

    Fix bug in login functionality

---
+++ lib/screens/widgets/main_product_widgets.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MainProductWidget extends StatelessWidget {
+  const MainProductWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
