843c4393e2886ad95b54b9a884678719c59e2ea3
Naman Gayakwad <110586020+Naman-Gayakwad@users.noreply.github.com>
2024-05-16 13:13:55 +0530

    feat: Add 'Recent Posts' section to home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -24,6 +24,13 @@ class _HomeState extends State<Home> {
           const SizedBox(
             height: 10,
           ),
+          Text(
+            'Recent Posts',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
         ],
       ),
     );
